0aef91125db10ba3339ecf1d84c78ccef920b141
Sheetal Godara <sheetalgodara@Jaideeps-MacBook-Pro.local>
2023-10-24 16:44:31 +0530

    removed unwanted more file

--- middleware/error.js
+++ middleware/error.js
@@ -1,6 +1,5 @@
-const logger = require('../startup/logging');
 module.exports = function(err,req,res,next){
 	// error warn info verbose debug silly->message and err object stak trace
-	logger.log('error',err.message,err);
+	console.log('error',err.message,err);
 	res.status(500).send(err);//500 for internal mongodb error can be changed to error .message or err
 }
